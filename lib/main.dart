7648d83ffc3b9d76070beb73099efb0d6aaef5b4
loveleenkaurnancy <loveleen.nancy@gmail.com>
2020-10-12 22:38:08 +0530

    Text Style Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,42 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title : "Sample App",
+      theme: ThemeData(
+        primarySwatch: Colors.red,
       ),
-    ),
-  );
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Flutter"
+          ),
+        ),
+        body: Center(
+          child: Text(
+            "I am Android Developer!",
+            textAlign: TextAlign.right,
+            style: TextStyle(
+              fontSize: 40.0,
+              fontStyle: FontStyle.italic,
+              color: Colors.red,
+              fontWeight: FontWeight.bold,
+              backgroundColor: Colors.yellow,
+              wordSpacing: 20.0,
+              decoration: TextDecoration.lineThrough,
+              decorationColor: Colors.green,
+              decorationStyle: TextDecorationStyle.dashed,
+              fontFamily: 'Raleway',
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
