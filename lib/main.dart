54d832029c9c41b5bf4e0500713cf00cbb73bc04
loveleenkaurnancy <loveleen.nancy@gmail.com>
2020-10-15 23:54:05 +0530

    Scaffold class

--- lib/main.dart
+++ lib/main.dart
@@ -5,55 +5,43 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+
+  static const String _title = 'Flutter Code Sample';
+
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      title : "Sample App",
-      theme: ThemeData(
-        primarySwatch: Colors.red,
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+
+  }
+}
+
+/// This is the stateful widget that the main application instantiates.
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _count = 0;
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Scaffold class'),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Flutter"
-          ),
-        ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              Image.asset('assets/images/pic.png'),
-            ],
-          ),
-
-          // TextField(
-          //   onChanged: (text) {
-          //     print("First text field: $text");
-          //   },
-          //   obscureText: false,
-          //   decoration: InputDecoration(
-          //     border: OutlineInputBorder(),
-          //     labelText: 'Enter Name',
-          //   ),
-          // )
-
-
-          // Text(
-          //   "I am Android Developer!",
-          //   textAlign: TextAlign.right,
-          //   style: TextStyle(
-          //     fontSize: 40.0,
-          //     fontStyle: FontStyle.italic,
-          //     color: Colors.red,
-          //     fontWeight: FontWeight.bold,
-          //     backgroundColor: Colors.yellow,
-          //     wordSpacing: 20.0,
-          //     decoration: TextDecoration.lineThrough,
-          //     decorationColor: Colors.green,
-          //     decorationStyle: TextDecorationStyle.dashed,
-          //     fontFamily: 'Raleway',
-          //   ),
-          // ),
-        ),
+      body: Center(child: Text('You have pressed the button $_count times.')),
+      backgroundColor: Colors.blueGrey.shade200,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => setState(() => _count++),
+        tooltip: 'Increment Counter',
+        child: const Icon(Icons.add),
       ),
     );
   }
