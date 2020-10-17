fbae4cbbb605f929a86fad62a3494fb311779a89
loveleenkaurnancy <loveleen.nancy@gmail.com>
2020-10-17 23:31:14 +0530

    Login Screen-II

--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,145 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
 
-  static const String _title = 'Flutter Code Sample';
+class _State extends State<MyApp> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    int _count = 0;
 
-    return MaterialApp(
-      title: _title,
-      home: MyStatefulWidget(),
-    );
-
-  }
-}
-
-/// This is the stateful widget that the main application instantiates.
-class MyStatefulWidget extends StatefulWidget {
-  MyStatefulWidget({Key key}) : super(key: key);
-
-  @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-}
-
-/// This is the private State class that goes with MyStatefulWidget.
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _count = 0;
-
-  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Scaffold class'),
+        title: const Text('Sample Code'),
       ),
-        body: Padding(
-            padding: EdgeInsets.all(15),
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(15),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'User Name',
-                      hintText: 'Enter Your Name',
-                    ),
-                  ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                controller: nameController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'User Name',
                 ),
-                Padding(
-                  padding: EdgeInsets.all(15),
-                  child: TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Password',
-                      hintText: 'Enter Password',
-                    ),
-                  ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                controller: nameController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'User Name',
                 ),
-                Padding(
-                  padding: EdgeInsets.all(15),
-                  child: MaterialButton(
-                    minWidth: double.infinity,
-                    textColor: Colors.white,
-                    color: Colors.blue,
-                    child: Text('Sign In'),
-                    onPressed: (){},
-                ),),
-              ],
-            )
-        )
+              ),
+            ),
+            Container(
+              width: 5.0,
+              height: 0,
+            ),
+            FlatButton(
+              onPressed: () {
+                //forgot password screen
+              },
+              textColor: Colors.blue,
+              child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('Forgot Password')),
+            ),
+          ],
+        ),
+      ),
     );
   }
+
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //       body: Padding(
+  //           padding: EdgeInsets.all(10),
+  //           child: Column(
+  //             mainAxisSize: MainAxisSize.max,
+  //             children: <Widget>[
+  //               Container(
+  //                   alignment: Alignment.center,
+  //                   padding: EdgeInsets.all(10),
+  //                   child: Text(
+  //                     'Sign in',
+  //                     style: TextStyle(fontSize: 20),
+  //                   )),
+  //               Container(
+  //                 padding: EdgeInsets.all(10),
+  //                 child: TextField(
+  //                   controller: nameController,
+  //                   decoration: InputDecoration(
+  //                     border: OutlineInputBorder(),
+  //                     labelText: 'User Name',
+  //                   ),
+  //                 ),
+  //               ),
+  //               Container(
+  //                 padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+  //                 child: TextField(
+  //                   obscureText: true,
+  //                   controller: passwordController,
+  //                   decoration: InputDecoration(
+  //                     border: OutlineInputBorder(),
+  //                     labelText: 'Password',
+  //                   ),
+  //                 ),
+  //               ),
+  //               FlatButton(
+  //                 onPressed: () {
+  //                   //forgot password screen
+  //                 },
+  //                 textColor: Colors.blue,
+  //                 child: Align(
+  //                     alignment: Alignment.centerLeft,
+  //                     child: Text('Forgot Password')),
+  //               ),
+  //               Container(
+  //                   height: 50,
+  //                   padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+  //                   child: RaisedButton(
+  //                     textColor: Colors.white,
+  //                     color: Colors.blue,
+  //                     child: Text('Login'),
+  //                     onPressed: () {
+  //                       print(nameController.text);
+  //                       print(passwordController.text);
+  //                     },
+  //                   )),
+  //               Align(alignment: Alignment.topLeft,
+  //                   child: Row(
+  //                     children: <Widget>[
+  //                       Text('First Time? ',
+  //                         style: TextStyle(
+  //                             fontSize: 15.0),
+  //                       ),
+  //                       Text(
+  //                         'Create Account',
+  //                         style: TextStyle(
+  //                             fontSize: 15.0, decoration: TextDecoration.underline),
+  //                       ),
+  //                     ],
+  //                     mainAxisAlignment: MainAxisAlignment.center,
+  //                   ))
+  //             ],
+  //           )));
+  // }
+
 }
