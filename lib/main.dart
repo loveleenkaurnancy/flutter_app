87c3a3785891dd7634a4feec0768775e09963eae
loveleenkaurnancy <loveleen.nancy@gmail.com>
2020-10-24 23:30:05 +0530

    ListView

--- lib/main.dart
+++ lib/main.dart
@@ -29,36 +29,40 @@ class _State extends State<MyApp> {
           decoration: BoxDecoration(
             color: Colors.black26,
           ),
-          child: GridView.count(
-            crossAxisCount: 2 ,
-            children: List.generate(10,(index){
-              return Container(
-                child: Card(
-                  color: Colors.blue,
-                ),
-              );
-            }),
-          )
+          child: _buildList()
         ),
       ),
     );
   }
 }
 
-Widget _buildDecoratedImage(int imageIndex) => Expanded(
-  child: Container(
-    decoration: BoxDecoration(
-      border: Border.all(width: 10, color: Colors.black38),
-      borderRadius: const BorderRadius.all(const Radius.circular(8)),
-    ),
-    margin: const EdgeInsets.all(4),
-    child: Image.asset('assets/images/pic.png'),
-  ),
+Widget _buildList() => ListView(
+  children: [
+    _tile('CineArts at the Empire', '85 W Portal Ave', Icons.theaters),
+    _tile('The Castro Theater', '429 Castro St', Icons.theaters),
+    _tile('Alamo Drafthouse Cinema', '2550 Mission St', Icons.theaters),
+    _tile('Roxie Theater', '3117 16th St', Icons.theaters),
+    _tile('United Artists Stonestown Twin', '501 Buckingham Way',
+        Icons.theaters),
+    _tile('AMC Metreon 16', '135 4th St #3000', Icons.theaters),
+    Divider(),
+    _tile('K\'s Kitchen', '757 Monterey Blvd', Icons.restaurant),
+    _tile('Emmy\'s Restaurant', '1923 Ocean Ave', Icons.restaurant),
+    _tile(
+        'Chaiya Thai Restaurant', '272 Claremont Blvd', Icons.restaurant),
+    _tile('La Ciccia', '291 30th St', Icons.restaurant),
+  ],
 );
 
-Widget _buildImageRow(int imageIndex) => Row(
-  children: [
-    _buildDecoratedImage(imageIndex),
-    _buildDecoratedImage(imageIndex + 1),
-  ],
+ListTile _tile(String title, String subtitle, IconData icon) => ListTile(
+  title: Text(title,
+      style: TextStyle(
+        fontWeight: FontWeight.w500,
+        fontSize: 20,
+      )),
+  subtitle: Text(subtitle),
+  leading: Icon(
+    icon,
+    color: Colors.blue[500],
+  ),
 );
