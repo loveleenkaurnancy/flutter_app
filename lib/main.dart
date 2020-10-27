39711717f9978ee4448a83e11f678e5f77046906
loveleenkaurnancy <loveleen.nancy@gmail.com>
2020-10-27 22:31:30 +0530

    ListTile

--- lib/main.dart
+++ lib/main.dart
@@ -29,12 +29,64 @@ class _State extends State<MyApp> {
           decoration: BoxDecoration(
             color: Colors.black26,
           ),
-          child: _buildCard()
+          child: _buildListView()
         ),
       ),
     );
   }
 }
+
+Widget _buildListView() => SizedBox(
+  child: ListView(
+    children: const <Widget>[
+      Card(child: ListTile(title: Text('One-line ListTile'))),
+      Card(
+        child: ListTile(
+          leading: FlutterLogo(),
+          title: Text('One-line with leading widget'),
+        ),
+      ),
+      Card(
+        child: ListTile(
+          title: Text('One-line with trailing widget'),
+          trailing: Icon(Icons.more_vert),
+        ),
+      ),
+      Card(
+        child: ListTile(
+          leading: FlutterLogo(),
+          title: Text('One-line with both widgets'),
+          trailing: Icon(Icons.more_vert),
+        ),
+      ),
+      Card(
+        child: ListTile(
+          title: Text('One-line dense ListTile'),
+          dense: true,
+        ),
+      ),
+      Card(
+        child: ListTile(
+          leading: FlutterLogo(size: 56.0),
+          title: Text('Two-line ListTile'),
+          subtitle: Text('Here is a second line'),
+          trailing: Icon(Icons.more_vert),
+        ),
+      ),
+      Card(
+        child: ListTile(
+          leading: FlutterLogo(size: 72.0),
+          title: Text('Three-line ListTile'),
+          subtitle: Text(
+              'A sufficiently long subtitle warrants three lines.'
+          ),
+          trailing: Icon(Icons.more_vert),
+          isThreeLine: true,
+        ),
+      ),
+    ],
+  ),
+);
 
 Widget _buildCard() => SizedBox(
   height: 210,
