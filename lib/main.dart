55db5619f5429974a606ec6c99d710cbdc40bebb
loveleenkaurnancy <loveleen.nancy@gmail.com>
2020-10-28 22:31:11 +0530

    Button

--- lib/main.dart
+++ lib/main.dart
@@ -12,113 +12,34 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
-  TextEditingController nameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    int _count = 0;
-
     return MaterialApp(
-      title: 'Flutter layout demo',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter layout demo'),
-        ),
-        body: Container(
-          decoration: BoxDecoration(
-            color: Colors.black26,
+          appBar: AppBar(
+            title: Text('Flutter FlatButton Example'),
           ),
-          child: _buildListView()
-        ),
+          body: Center(child: Column(children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(25),
+              child: FlatButton(
+                child: Text('SignUp', style: TextStyle(fontSize: 20.0),),
+                onPressed: () {},
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(25),
+              child: FlatButton(
+                child: Text('LogIn', style: TextStyle(fontSize: 20.0),),
+                color: Colors.blueAccent,
+                textColor: Colors.white,
+                onPressed: () {},
+              ),
+            ),
+          ]
+          ))
       ),
     );
   }
 }
-
-Widget _buildListView() => SizedBox(
-  child: ListView(
-    children: const <Widget>[
-      Card(child: ListTile(title: Text('One-line ListTile'))),
-      Card(
-        child: ListTile(
-          leading: FlutterLogo(),
-          title: Text('One-line with leading widget'),
-        ),
-      ),
-      Card(
-        child: ListTile(
-          title: Text('One-line with trailing widget'),
-          trailing: Icon(Icons.more_vert),
-        ),
-      ),
-      Card(
-        child: ListTile(
-          leading: FlutterLogo(),
-          title: Text('One-line with both widgets'),
-          trailing: Icon(Icons.more_vert),
-        ),
-      ),
-      Card(
-        child: ListTile(
-          title: Text('One-line dense ListTile'),
-          dense: true,
-        ),
-      ),
-      Card(
-        child: ListTile(
-          leading: FlutterLogo(size: 56.0),
-          title: Text('Two-line ListTile'),
-          subtitle: Text('Here is a second line'),
-          trailing: Icon(Icons.more_vert),
-        ),
-      ),
-      Card(
-        child: ListTile(
-          leading: FlutterLogo(size: 72.0),
-          title: Text('Three-line ListTile'),
-          subtitle: Text(
-              'A sufficiently long subtitle warrants three lines.'
-          ),
-          trailing: Icon(Icons.more_vert),
-          isThreeLine: true,
-        ),
-      ),
-    ],
-  ),
-);
-
-Widget _buildCard() => SizedBox(
-  height: 210,
-  child: Card(
-    child: Column(
-      children: [
-        ListTile(
-          title: Text('1625 Main Street',
-              style: TextStyle(fontWeight: FontWeight.w500)),
-          subtitle: Text('My City, CA 99984'),
-          leading: Icon(
-            Icons.restaurant_menu,
-            color: Colors.blue[500],
-          ),
-        ),
-        Divider(),
-        ListTile(
-          title: Text('(408) 555-1212',
-              style: TextStyle(fontWeight: FontWeight.w500)),
-          leading: Icon(
-            Icons.contact_phone,
-            color: Colors.blue[500],
-          ),
-        ),
-        ListTile(
-          title: Text('costa@example.com'),
-          leading: Icon(
-            Icons.contact_mail,
-            color: Colors.blue[500],
-          ),
-        ),
-      ],
-    ),
-  ),
-);
