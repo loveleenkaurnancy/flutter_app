87f2fe251d545d8872a6990d579706a0d0715516
loveleenkaurnancy <loveleen.nancy@gmail.com>
2020-11-02 22:51:48 +0530

    Circular Progress

--- lib/main.dart
+++ lib/main.dart
@@ -14,80 +14,25 @@ class MyApp extends StatefulWidget {
 
 class _State extends State<MyApp> {
 
-  void showToast() {
-    Fluttertoast.showToast(
-        msg: 'This is toast notification',
-        toastLength: Toast.LENGTH_SHORT,
-        gravity: ToastGravity.BOTTOM,
-        timeInSecForIos: 1,
-        backgroundColor: Colors.red,
-        textColor: Colors.yellow
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
-            title: Text('Flutter FlatButton Example'),
+            title: Text('Flutter'),
           ),
-          body: MyCustomForm(),
+          body: CircularProgressIndicatorApp(),
       ),
     );
   }
 }
 
-// Create a Form widget.
-class MyCustomForm extends StatefulWidget {
-  @override
-  MyCustomFormState createState() {
-    return MyCustomFormState();
-  }
-}
-// Create a corresponding State class. This class holds data related to the form.
-class MyCustomFormState extends State<MyCustomForm> {
-  // Create a global key that uniquely identifies the Form widget
-  // and allows validation of the form.
-  final _formKey = GlobalKey<FormState>();
-
+/// This is the stateless widget that the main application instantiates.
+class CircularProgressIndicatorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Build a Form widget using the _formKey created above.
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          TextFormField(
-            decoration: const InputDecoration(
-              icon: const Icon(Icons.person),
-              hintText: 'Enter your name',
-              labelText: 'Name',
-            ),
-          ),
-          TextFormField(
-            decoration: const InputDecoration(
-              icon: const Icon(Icons.phone),
-              hintText: 'Enter a phone number',
-              labelText: 'Phone',
-            ),
-          ),
-          TextFormField(
-            decoration: const InputDecoration(
-              icon: const Icon(Icons.calendar_today),
-              hintText: 'Enter your date of birth',
-              labelText: 'Dob',
-            ),
-          ),
-          new Container(
-              padding: const EdgeInsets.only(left: 150.0, top: 40.0),
-              child: new RaisedButton(
-                child: const Text('Submit'),
-                onPressed: null,
-              )),
-        ],
-      ),
-    );
+    return CircularProgressIndicator(
+      backgroundColor: Colors.red,
+      strokeWidth: 8,);
   }
 }
