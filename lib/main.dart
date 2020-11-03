6720df9356b2e0b77fb3f1883f872b11bb2ebe24
loveleenkaurnancy <loveleen.nancy@gmail.com>
2020-11-03 22:18:35 +0530

    Alert Dialog

--- lib/main.dart
+++ lib/main.dart
@@ -21,18 +21,50 @@ class _State extends State<MyApp> {
           appBar: AppBar(
             title: Text('Flutter'),
           ),
-          body: CircularProgressIndicatorApp(),
+          body: MyAlert(),
       ),
     );
   }
 }
 
-/// This is the stateless widget that the main application instantiates.
-class CircularProgressIndicatorApp extends StatelessWidget {
+class MyAlert extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return CircularProgressIndicator(
-      backgroundColor: Colors.red,
-      strokeWidth: 8,);
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: RaisedButton(
+        child: Text('Show alert'),
+        onPressed: () {
+          showAlertDialog(context);
+        },
+      ),
+    );
   }
+}
+
+showAlertDialog(BuildContext context) {
+  // Create button
+  Widget okButton = FlatButton(
+    child: Text("OK"),
+    onPressed: () {
+      Navigator.of(context).pop();
+    },
+  );
+
+  // Create AlertDialog
+  AlertDialog alert = AlertDialog(
+    title: Text("Simple Alert"),
+    content: Text("This is an alert message."),
+    actions: [
+      okButton,
+    ],
+  );
+
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
 }
