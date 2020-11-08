06e4f472db1c24458365a33ebbdc7bb0cf4aca16
loveleenkaurnancy <loveleen.nancy@gmail.com>
2020-11-08 23:07:19 +0530

    Slider

--- lib/main.dart
+++ lib/main.dart
@@ -13,20 +13,47 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
-  var lists = List<String>.generate(11, (index) => "List : $index");
+  int _value = 6;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter SnackBar Demo'),
       ),
-      body: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            Icon(Icons.camera_enhance),
-            Icon(Icons.camera_front),
-            Icon(Icons.camera_rear),
-          ]),
+      body: Padding(
+        padding: EdgeInsets.all(15.0),
+        child: Center(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Icon(
+                    Icons.volume_up,
+                    size: 40,
+                  ),
+                  new Expanded(
+                      child: Slider(
+                          value: _value.toDouble(),
+                          min: 1.0,
+                          max: 20.0,
+                          divisions: 10,
+                          activeColor: Colors.green,
+                          inactiveColor: Colors.orange,
+                          label: 'Set volume value',
+                          onChanged: (double newValue) {
+                            setState(() {
+                              _value = newValue.round();
+                            });
+                          },
+                          semanticFormatterCallback: (double newValue) {
+                            return '${newValue.round()} dollars';
+                          }
+                      )
+                  ),
+                ]
+            )
+        ),
+      )
     );
   }
 
