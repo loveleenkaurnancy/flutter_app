3fccb8157f4253244213b6b80bb2e5ff792fc56f
loveleenkaurnancy <loveleen.nancy@gmail.com>
2020-11-11 23:46:43 +0530

    Animation

--- lib/main.dart
+++ lib/main.dart
@@ -11,49 +11,18 @@ class MyApp extends StatefulWidget {
   _State createState() => _State();
 }
 
-class _State extends State<MyApp> {
-  int _value = 6;
+class _State extends State<MyApp> {=
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter SnackBar Demo'),
+        title: Text('Flutter Animation'),
       ),
-      body: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children:<Widget>[
-            Container(
-              margin: EdgeInsets.all(10),
-              child: Tooltip(
-                  waitDuration: Duration(seconds: 1),
-                  showDuration: Duration(seconds: 2),
-                  padding: EdgeInsets.all(5),
-                  height: 35,
-                  textStyle: TextStyle(
-                      fontSize: 15, color: Colors.white, fontWeight: FontWeight.normal),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10), color: Colors.green),
-                  message: 'My Account',
-                  child: FlatButton(
-                    child: Icon(
-                      Icons.account_box,
-                      size: 100,
-                    ),
-                  )),
-            ),
-            Container(
-              margin: EdgeInsets.all(10),
-              child: Tooltip(
-                  message: 'My Account',
-                  child: FlatButton(
-                    child: Icon(
-                      Icons.account_box,
-                      size: 100,
-                    ),
-                  )
-              ),
-            )
-          ]
+      body: Container(
+        margin: EdgeInsets.symmetric(vertical: 10),
+        height: 300,
+        width: 300,
+        child: FlutterLogo(),
       ),
     );
   }
