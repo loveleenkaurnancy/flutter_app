7b6541e829c68e2570d7cdbb87ab485e804da46e
loveleenkaurnancy <loveleen.nancy@gmail.com>
2020-11-12 22:48:22 +0530

    Table

--- lib/main.dart
+++ lib/main.dart
@@ -11,19 +11,50 @@ class MyApp extends StatefulWidget {
   _State createState() => _State();
 }
 
-class _State extends State<MyApp> {=
+class _State extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter Animation'),
-      ),
-      body: Container(
-        margin: EdgeInsets.symmetric(vertical: 10),
-        height: 300,
-        width: 300,
-        child: FlutterLogo(),
-      ),
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Flutter Table Example'),
+          ),
+          body: Center(
+              child: Column(children: <Widget>[
+                Container(
+                  margin: EdgeInsets.all(20),
+                  child: Table(
+                    defaultColumnWidth: FixedColumnWidth(120.0),
+                    border: TableBorder.all(
+                        color: Colors.black,
+                        style: BorderStyle.solid,
+                        width: 2),
+                    children: [
+                      TableRow( children: [
+                        Column(children:[Text('Website', style: TextStyle(fontSize: 20.0))]),
+                        Column(children:[Text('Tutorial', style: TextStyle(fontSize: 20.0))]),
+                        Column(children:[Text('Review', style: TextStyle(fontSize: 20.0))]),
+                      ]),
+                      TableRow( children: [
+                        Column(children:[Text('Javatpoint')]),
+                        Column(children:[Text('Flutter')]),
+                        Column(children:[Text('5*')]),
+                      ]),
+                      TableRow( children: [
+                        Column(children:[Text('Javatpoint')]),
+                        Column(children:[Text('MySQL')]),
+                        Column(children:[Text('5*')]),
+                      ]),
+                      TableRow( children: [
+                        Column(children:[Text('Javatpoint')]),
+                        Column(children:[Text('ReactJS')]),
+                        Column(children:[Text('5*')]),
+                      ]),
+                    ],
+                  ),
+                ),
+              ])
+          )),
     );
   }
 }
