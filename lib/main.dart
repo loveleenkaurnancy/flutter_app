e76efd8363ef4cf37bcd61e1f4536c22b0c5bc94
loveleenkaurnancy <loveleen.nancy@gmail.com>
2020-11-18 23:15:00 +0530

    Themes

--- lib/main.dart
+++ lib/main.dart
@@ -9,66 +9,51 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyNavigationBar(),
+      theme: ThemeData(
+        // Define the default brightness and colors.
+        brightness: Brightness.dark,
+        primaryColor: Colors.lightBlue,
+        accentColor: Colors.green,
+
+        // Define the default font family.
+        fontFamily: 'Monotype Coursiva',
+
+        // Define the TextTheme that specifies the default
+        // text styling for headlines, titles, bodies of text, and more.
+        textTheme: TextTheme(
+            headline: TextStyle(fontSize: 32.0, fontStyle: FontStyle.italic, fontFamily: 'Hind')
+        ),
+      ),
+      home: MyThemePage(),
     );
   }
 }
 
-class MyNavigationBar extends StatefulWidget {
-  MyNavigationBar ({Key key}) : super(key: key);
-
-  @override
-  _MyNavigationBarState createState() => _MyNavigationBarState();
-}
-
-class _MyNavigationBarState extends State<MyNavigationBar > {
-  int _selectedIndex = 0;
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text('Home Page', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
-    Text('Search Page', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
-    Text('Profile Page', style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
+class MyThemePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: const Text('Flutter BottomNavigationBar Example'),
-          backgroundColor: Colors.green
+        title: Text('Flutter Theme Example'),
       ),
       body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
+        child: Container(
+          color: Theme.of(context).accentColor,
+          child: Text(
+            'Themes contains the graphical appearances that makes the user interface more attractive.',
+            style: Theme.of(context).textTheme.headline,
+          ),
+        ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                title: Text('Home'),
-                backgroundColor: Colors.green
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.search),
-                title: Text('Search'),
-                backgroundColor: Colors.yellow
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              title: Text('Profile'),
-              backgroundColor: Colors.blue,
-            ),
-          ],
-          type: BottomNavigationBarType.shifting,
-          currentIndex: _selectedIndex,
-          selectedItemColor: Colors.black,
-          iconSize: 40,
-          onTap: _onItemTapped,
-          elevation: 5
+      floatingActionButton: Theme(
+        data: Theme.of(context).copyWith(
+          colorScheme:
+          Theme.of(context).colorScheme.copyWith(secondary: Colors.blue),
+        ),
+        child: FloatingActionButton(
+          onPressed: null,
+          child: Icon(Icons.person),
+        ),
       ),
     );
   }
