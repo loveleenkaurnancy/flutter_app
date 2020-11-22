f7467274b5a033ff7c074492a6fc92c570dc4680
loveleenkaurnancy <loveleen.nancy@gmail.com>
2020-11-22 23:15:55 +0530

    ToggleButtons

--- lib/main.dart
+++ lib/main.dart
@@ -5,74 +5,39 @@ import 'package:splashscreen/splashscreen.dart';
 void main() => runApp(MyApp());
 
 /// This Widget is the main application widget.
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<bool> _selections = List.generate(3, (_) => false);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      home: SplashScreenPage(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
-class SplashScreenPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return SplashScreen(
-      seconds: 5,
-      navigateAfterSeconds: new HomeScreen(),
-      backgroundColor: Colors.yellow,
-      title: new Text('Flutter Javatpoint',textScaleFactor: 2,),
-      image: new Image.network(
-          'https://static.javatpoint.com/tutorial/flutter/images/flutter-creating-android-platform-specific-code3.png'
-      ),
-      loadingText: Text("Loading"),
-      photoSize: 150.0,
-      loaderColor: Colors.red,
-    );
-  }
-}
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+        home: Scaffold(
           appBar: AppBar(
-            title: Text('Flutter Tutorial - TutorialKart'),
+            title: Center(child: Text('Flutter - tutorialkart.com')),
           ),
           body: ListView(children: <Widget>[
-            Center(
-                child: Text(
-                  'Students',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                )),
-            DataTable(
-              columns: [
-                DataColumn(label: Text('RollNo')),
-                DataColumn(label: Text('Name')),
-                DataColumn(label: Text('Class')),
-              ],
-              rows: [
-                DataRow(cells: [
-                  DataCell(Text('1')),
-                  DataCell(Text('Arya')),
-                  DataCell(Text('6')),
-                ]),
-                DataRow(cells: [
-                  DataCell(Text('12')),
-                  DataCell(Text('John')),
-                  DataCell(Text('9')),
-                ]),
-                DataRow(cells: [
-                  DataCell(Text('42')),
-                  DataCell(Text('Tony')),
-                  DataCell(Text('8')),
-                ]),
-              ],
-            ),
-          ])),
-    );
+            Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.all(20),
+                child: ToggleButtons(
+                  children: <Widget>[
+                    Icon(Icons.add_comment),
+                    Icon(Icons.airline_seat_individual_suite),
+                    Icon(Icons.add_location),
+                  ],
+                  isSelected: _selections,
+                  onPressed: (int index) {
+                    setState(() {
+                      _selections[index] = !_selections[index];
+                    });
+                  },
+                ))
+          ]),
+        ));
   }
 }
